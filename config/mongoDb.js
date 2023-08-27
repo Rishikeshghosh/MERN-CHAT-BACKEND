4b5a35ffd53b33889484f4fdeacd54efe88044ed
Rishikeshghosh <rishikeshghoshghosh@gmail.com>
2023-08-27 13:37:42 +0530

    changed some codes in databsae connection

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -4,15 +4,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/MERN-CHAT", {
-      //process.env.MONGO_URL
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB connecteed: ${conn.connection.host}`.cyan.underline);
-  } catch (err) {
-    console.error(`Error : ${err}`.red);
-    process.exit(1);
+    const connect = mongoose.connect(process.env.MONGO_URL);
+    console.log(`DATABASE CONNECTED`);
+  } catch (error) {
+    console.log(error);
   }
 };
 module.exports = connectDB;
