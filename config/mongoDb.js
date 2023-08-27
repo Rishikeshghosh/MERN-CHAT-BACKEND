a397ed47229dd023755506de50a9a863e40b0dcb
Rishikeshghosh <rishikeshghoshghosh@gmail.com>
2023-08-27 14:53:03 +0530

    h

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -5,7 +5,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://riptu:CakZuDMR3xsdKuRH@cluster0.agsjgzb.mongodb.net/mern-chat-app?retryWrites=true&w=majority"
+      "mongodb+srv://riptu:CakZuDMR3xsdKuRH@cluster0.agsjgzb.mongodb.net/mern-chat-app?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
     );
     console.log(`DATABASE CONNECTED ${conn.connection.host}`);
   } catch (error) {
