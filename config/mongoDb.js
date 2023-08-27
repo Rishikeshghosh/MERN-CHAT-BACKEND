1cf42aae7379df8589c04ef1856f57431cb909f4
Rishikeshghosh <rishikeshghoshghosh@gmail.com>
2023-08-27 14:23:22 +0530

    h

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connect = mongoose.connect(process.env.MONGO_URL);
+    const connect = mongoose.connect(
+      "mongodb+srv://rishikeshghoshghosh:XsqDFY6y7irQbbfh@cluster0.kuz3zjp.mongodb.net/mern-chat-app?retryWrites=true&w=majority"
+    );
     console.log(`DATABASE CONNECTED`);
   } catch (error) {
     console.log(error);
